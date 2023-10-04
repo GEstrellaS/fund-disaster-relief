c143d8b00e3d9eb2fae0186a4cd7a3546940538b
ge7659 <ge7659@rit.edu>
2023-10-04 13:56:27 -0400

    Updated NeedDAO.java documentation and created needs.json

--- ufund-api/src/main/java/com/persistance/NeedDAO.java
+++ ufund-api/src/main/java/com/persistance/NeedDAO.java
@@ -10,33 +10,33 @@ import com.model.Need;
  */
 public interface NeedDAO {
     /**
-     * Retrieves all {@linkplain Hero heroes}
+     * Retrieves all {@linkplain Need needs}
      * 
-     * @return An array of {@link Hero hero} objects, may be empty
+     * @return An array of {@link Need need} objects, may be empty
      * 
      * @throws IOException if an issue with underlying storage
      */
     Need[] getNeeds() throws IOException;
 
     /**
-     * Finds all {@linkplain Hero heroes} whose name contains the given text
+     * Finds all {@linkplain Need needs} whose name contains the given text
      * 
      * @param containsText The text to match against
      * 
-     * @return An array of {@link Hero heroes} whose nemes contains the given text, may be empty
+     * @return An array of {@link Need needs} whose nemes contains the given text, may be empty
      * 
      * @throws IOException if an issue with underlying storage
      */
     Need[] findNeeds(String containsText) throws IOException;
 
     /**
-     * Retrieves a {@linkplain Hero hero} with the given id
+     * Retrieves a {@linkplain Need need} with the given id
      * 
-     * @param id The id of the {@link Hero hero} to get
+     * @param id The name of the {@link Need need} to get
      * 
-     * @return a {@link Hero hero} object with the matching id
+     * @return a {@link Need need} object with the matching name
      * <br>
-     * null if no {@link Hero hero} with a matching id is found
+     * null if no {@link Need need} with a matching name is found
      * 
      * @throws IOException if an issue with underlying storage
      */
@@ -45,36 +45,36 @@ public interface NeedDAO {
     /**
      * Creates and saves a {@linkplain Hero hero}
      * 
-     * @param hero {@linkplain Hero hero} object to be created and saved
+     * @param hero {@linkplain Need need} object to be created and saved
      * <br>
-     * The id of the hero object is ignored and a new uniqe id is assigned
+     * The name of the hero object is ignored and a new uniqe name is assigned
      *
-     * @return new {@link Hero hero} if successful, false otherwise 
+     * @return new {@link Need need} if successful, false otherwise 
      * 
      * @throws IOException if an issue with underlying storage
      */
     Need createNeed(Need need) throws IOException;
 
     /**
-     * Updates and saves a {@linkplain Hero hero}
+     * Updates and saves a {@linkplain Need need}
      * 
-     * @param {@link Hero hero} object to be updated and saved
+     * @param {@link Need need} object to be updated and saved
      * 
-     * @return updated {@link Hero hero} if successful, null if
-     * {@link Hero hero} could not be found
+     * @return updated {@link Need need} if successful, null if
+     * {@link Need need} could not be found
      * 
      * @throws IOException if underlying storage cannot be accessed
      */
     Need updateNeed(Need need) throws IOException;
 
     /**
-     * Deletes a {@linkplain Hero hero} with the given id
+     * Deletes a {@linkplain Need need} with the given id
      * 
-     * @param id The id of the {@link Hero hero}
+     * @param name The name of the {@link Need need}
      * 
-     * @return true if the {@link Hero hero} was deleted
+     * @return true if the {@link Need need} was deleted
      * <br>
-     * false if hero with the given id does not exist
+     * false if hero with the given name does not exist
      * 
      * @throws IOException if underlying storage cannot be accessed
      */
