79a9d4e7e34391345c4b4d76bae9a3e1f83cda78
Ansley Orrell <alo4583@rit.edu>
2023-10-18 22:31:00 -0400

    Fixed the updateNeed test

--- ufund-api/src/test/java/com/ufund/api/ufundapi/controller/NeedControllerTest.java
+++ ufund-api/src/test/java/com/ufund/api/ufundapi/controller/NeedControllerTest.java
@@ -1,17 +1,13 @@
 package com.ufund.api.ufundapi.controller;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-import java.io.File;
 import java.io.IOException;
 
 import com.ufund.api.ufundapi.persistence.NeedDAO;
-import com.ufund.api.ufundapi.persistence.NeedFileDAO;
-import com.fasterxml.jackson.core.exc.StreamReadException;
-import com.fasterxml.jackson.databind.DatabindException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.model.Need;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -29,8 +25,6 @@ import org.springframework.http.ResponseEntity;
 public class NeedControllerTest {
     private NeedController needController;
     private NeedDAO mockNeedDAO;
-    private ObjectMapper mockObjectMapper;
-    private Need[] testNeeds;
 
     /**
      * Before each test, create a new NeedController object and inject
@@ -39,18 +33,6 @@ public class NeedControllerTest {
      */
     @BeforeEach
     public void setupNeedController() throws IOException {
-        // mockObjectMapper = mock(ObjectMapper.class);
-        // testNeeds = new Need[3];
-        // testNeeds[0] = new Need("Toilet Paper", 4.99, 24, "Household");
-        // testNeeds[1] = new Need("Trashcan", 4.99, 24, "Bathroom");
-        // testNeeds[2] = new Need("Banana", 4.99, 24, "Food");
-
-        // // When the object mapper is supposed to read from the file
-        // // the mock object mapper will return the Need array above
-        // when(mockObjectMapper
-        //     .readValue(new File("doesnt_matter.txt"),Need[].class))
-        //         .thenReturn(testNeeds);
-        // mockNeedDAO = new NeedFileDAO("doesnt_matter.txt",mockObjectMapper);
         mockNeedDAO = mock(NeedDAO.class);
         needController = new NeedController(mockNeedDAO);
     }
@@ -89,19 +71,20 @@ public class NeedControllerTest {
     @Test
     public void testUpdateneed() throws IOException { // updateneed may throw IOException
         // Setup
-        Need need = new Need("Toilet Paper", 4.99, 24, "Household");
+        Need existingNeed = new Need("Toilet Paper", 4.99, 24, "Household");
+        Need needToUpdate = new Need("Toilet Paper", 5.99, 24, "Household");
         // when updateneed is called, return true simulating successful
         // update and save
-        when(mockNeedDAO.updateNeed(need)).thenReturn(need);
-        ResponseEntity<Need> response = needController.updateNeed(need);
-        need.setCost(5.99);
 
-        // Invoke
-        response = needController.updateNeed(need);
+        when(mockNeedDAO.getNeed(existingNeed.getName())).thenReturn(existingNeed);
+        when(mockNeedDAO.updateNeed(any(Need.class))).thenReturn(needToUpdate);
+
+        // Invoke 
+        ResponseEntity<Need> response = needController.updateNeed(needToUpdate);
 
         // Analyze
         assertEquals(HttpStatus.OK,response.getStatusCode());
-        assertEquals(need,response.getBody());
+        assertEquals(needToUpdate,response.getBody());
     }
 
     @Test
