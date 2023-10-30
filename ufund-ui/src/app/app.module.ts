284a1ec12e3464f11a8f217f17a36d5499bb99da
ge7659 <ge7659@rit.edu>
2023-10-30 03:24:29 -0400

    routing completed with a semi functional login page.

--- ufund-ui/src/app/app.module.ts
+++ ufund-ui/src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Add this import
+import { HttpClientModule } from '@angular/common/http'; // Add this import
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ufund-ui/src/app/login/login.component.ts
+++ ufund-ui/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+  username: string = "";
+  password: string = "";
+  errorMessage: string = '';
 
+  constructor(private router: Router, private http: HttpClient) {}
+
+  login() {
+    this.router.navigate(['/home']);
+  }
 }
