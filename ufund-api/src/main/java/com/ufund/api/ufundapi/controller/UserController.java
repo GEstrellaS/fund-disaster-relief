adb711537afefe469c9b84ab0dacd3f6aa0e560f
ge7659 <ge7659@rit.edu>
2023-11-01 04:22:45 -0400

    frontend login works with the backend login

--- ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ufund.api.ufundapi.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,7 @@ public class UserController {
         this.userDAO = userDAO;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/login")
 	public ResponseEntity<Users> loginUser(@RequestParam("username") String username, @RequestParam("password") String password){
 		LOG.info("GET /users/login?username=" + username + "&password=" + password);
