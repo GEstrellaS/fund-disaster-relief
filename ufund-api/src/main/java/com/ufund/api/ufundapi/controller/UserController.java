483644615088c21b6f99b5fb32433c2eb92a78c6
ge7659 <ge7659@rit.edu>
2023-11-01 03:02:26 -0400

    created the non functional backend for the users

---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/controller/UserController.java
@@ -0,0 +1,163 @@
+package com.ufund.api.ufundapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.model.DonationCart;
+import com.model.Need;
+import com.model.Users;
+import com.ufund.api.ufundapi.persistence.UserDAO;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("users") 
+public class UserController {
+    private static final Logger LOG = Logger.getLogger(UserController.class.getName());
+    private UserDAO userDAO;
+
+    
+    public UserController(UserDAO userDAO) {
+        this.userDAO = userDAO;
+    }
+
+
+    @GetMapping("/{username}")
+    public ResponseEntity<Users> getUser(@PathVariable String username) {
+        LOG.info("GET /users/" + username);
+        try {
+            Users user = userDAO.getUser(username);
+            if (user != null)
+                return new ResponseEntity<Users>(user,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+  
+    @GetMapping("")
+    public ResponseEntity<Users[]> getUsers() {
+        LOG.info("GET /users");
+
+        try {
+            Users[] user = userDAO.getUsers(null);
+            if (user!= null)
+                return new ResponseEntity<Users[]>(user,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Users[]> searchUsers(@RequestParam String username) {
+        LOG.info("GET /users/?username="+username);
+        try {
+            Users[] users = userDAO.getUsers(username);
+            if (users != null)
+                return new ResponseEntity<Users[]>(users,HttpStatus.OK);
+            else
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("")
+    public ResponseEntity<Users> createUser(@RequestBody Users user) {
+        LOG.info("POST /users " + user);
+        try {
+            Users checkUser = userDAO.getUser(user.getUsername());
+            if(checkUser != null){
+                return new ResponseEntity<>(HttpStatus.CONFLICT);
+            }
+
+            userDAO.createUser(user);
+            return new ResponseEntity<>(user, HttpStatus.CREATED);
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/{username}")
+    public ResponseEntity<Users> deleteUser(@PathVariable String username) {
+        LOG.info("DELETE /users/" + username);
+
+        try {
+            Users existingUsers = userDAO.getUser(username);
+            if (existingUsers != null){
+                boolean delete = userDAO.deleteUser(username);
+                if (delete){
+                  return new ResponseEntity<>(HttpStatus.OK);
+                }           
+                 else{
+                    return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+                }
+            }else {
+                 return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+                }
+        }
+        catch(IOException e) {
+            LOG.log(Level.SEVERE,e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/cart/{username}")
+	public ResponseEntity<DonationCart> getDonationCart(@PathVariable("username") String usernameString) {
+		LOG.info("GET /user/cart/" + usernameString);
+		try {
+			DonationCart cart = userDAO.getDonationCart(usernameString);
+			return new ResponseEntity<DonationCart>(cart, HttpStatus.OK);
+		} catch (IOException e) {
+			LOG.log(Level.SEVERE, e.getLocalizedMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+    @PostMapping("/cart/{username}/add/need")
+	public ResponseEntity<DonationCart> addItemToCart(@PathVariable("username") String usernameString, @RequestBody Need need) {
+		LOG.info("POST /users/" + "/cart/" + usernameString +"/add/" + need);
+		try {
+			DonationCart cart = userDAO.addItemToDonationCart(usernameString, need);
+			return new ResponseEntity<DonationCart>(cart, HttpStatus.OK);
+		} catch (IOException e) {
+			LOG.log(Level.SEVERE, e.getLocalizedMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+    @PostMapping("/cart/{username}/delete/need")
+	public ResponseEntity<DonationCart> deleteItemToCart(@PathVariable("username") String usernameString, @RequestBody Need need) {
+		LOG.info("POST /users/" + "/cart/" + usernameString +"/delete/" + need);
+		try {
+			DonationCart cart = userDAO.deleteItermToDonationCart(usernameString, need);
+			return new ResponseEntity<DonationCart>(cart, HttpStatus.OK);
+		} catch (IOException e) {
+			LOG.log(Level.SEVERE, e.getLocalizedMessage());
+			return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+}
---
+++ ufund-api/src/main/java/com/ufund/api/ufundapi/persistence/UserDAO.java
@@ -0,0 +1,25 @@
+package com.ufund.api.ufundapi.persistence;
+
+import java.io.IOException;
+
+import com.model.DonationCart;
+import com.model.Need;
+import com.model.Users;
+
+public interface UserDAO {
+
+    Users getUser(String name) throws IOException;
+
+    Users[] getUsers(String containsText) throws IOException;
+
+    Users createUser(Users user) throws IOException;
+
+    boolean deleteUser(String user) throws IOException;
+
+    DonationCart getDonationCart(String user) throws IOException;
+
+    DonationCart addItemToDonationCart(String user, Need need) throws IOException;
+
+    DonationCart deleteItermToDonationCart(String user, Need need) throws IOException;
+
+}
